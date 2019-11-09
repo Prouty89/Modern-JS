f0ab647b65cc087e57a88b68ba2b0e25ee25ba5b
Blake <bpro0821@gmail.com>
2019-11-08 22:56:27 -0600

    Through 6 Variables

---
+++ js_sandbox/app.js
@@ -0,0 +1,91 @@
+// Log to console
+
+// console.log('Hello Beautiful');
+// console.log(123)
+// console.log(true)
+
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+
+// console.error('This is some error');
+
+// console.clear();
+// console.warn('Warning');
+// console.time('Hello');
+// console.log('Hello Beautiful');
+// console.log('Hello Beautiful');
+// console.log('Hello Beautiful');
+// console.log('Hello Beautiful');
+// console.timeEnd('Hello');
+
+
+
+// var, let, const
+
+// var name = 'John Doe'; 
+
+// console.log(name);
+
+// name = 'Steve Bar';
+
+// console.log(name);
+
+// // Initialize a var
+
+// var greeting;
+// console.log(greeting); // undefined
+// greeting= 'Hello';
+// console.log(greeting); // Hello
+
+// // Variables only allow for letters, numbers, _, $
+// // Variables cannot start with a number
+
+// // Multi word vars 
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; //Snake case
+// var FirstName = 'Tom'; // Pascal case
+// var firstname; // not recommended
+
+
+// LET
+// let name;
+// name = 'John Doe'; 
+// console.log(name);
+// name = 'Steve Bar';
+// console.log(name);
+
+//CONST
+
+// const name = 'John';
+// console.log(name);
+// Can not reassign
+// name = 'Sara';  Assignment to a constant variable
+
+// Have to assign a value
+//const greeting; Missing initializer in const declaration
+
+const person = {
+    name: 'John',
+    age: 30
+}
+
+//You CAN reassign the values within an object.
+
+person.name = 'Sara';
+person.age = 32;
+
+// console.log(person);
+
+const numbers = [1,22,3,4,5];
+numbers.push(6);
+//const numbers = [1,22,3,4,5,6]; XXXXXXXX
+
+console.log(numbers);
+
+
+
+
+
