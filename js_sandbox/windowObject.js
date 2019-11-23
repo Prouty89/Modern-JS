ab7f5740bf06730c6abce4a697c43dc0fae74771
Blake <bpro0821@gmail.com>
2019-11-23 12:08:35 -0600

    Through JS Language Fundamentals

---
+++ js_sandbox/windowObject.js
@@ -0,0 +1,74 @@
+// Window object
+// Window is the global object in client side JS
+// Window/ browser is the global environment (v8)
+//Local storage/fetch API/ navigator object/ Alert/ Prompt/ confirm
+
+// Window methods/ Objects/ Properties
+
+console.log(123)
+
+// Alert
+
+// alert('Hello World')
+
+// PromptHello
+// const input = prompt();
+// alert(input);
+
+// Confirm
+
+// if(confirm('Are you sure')){
+//     console.log('Yes')
+// } else{
+//     console.log('No')
+// }
+
+// let val; 
+
+// Outer Height and Width
+
+// val = window.outerHeight;
+// val = window.outerWidth;
+
+// Inner Height and Width
+// val = window.innerHeight;
+// val = window.innerWidth;
+
+//Scroll points
+
+// val = window.scrollY;
+// val = window.scrollX;
+
+// Location Object
+
+// val = window.location;
+// val = window.location.hostname;
+// val = window.location.port;
+// val = window.location.href;
+// val = window.location.search;
+
+// // Redirect
+// window.location.href = 'http://google.com';
+
+// // Reload
+// window.location.reload();
+
+// History
+
+// window.history.go();
+// val = window.history.length;
+
+//Navigator Object
+
+// val = window.navigator;
+
+// val = window.navigator.appName;
+// val = window.navigator.appVersion;
+// val = window.navigator.userAgent;
+// val = window.navigator.platform;
+// val = window.navigator.vendor;
+// val = window.navigator.language;
+
+
+
+// console.log(val);
